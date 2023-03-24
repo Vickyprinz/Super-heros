4f41c4afee110ce8d7c54155c5c8eda5fbc71f95
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-24 14:09:12 +0300

    updated get request

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,8 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index 
+        heros = Hero.all 
+        render json: heros, status: :created
+    end
 end
