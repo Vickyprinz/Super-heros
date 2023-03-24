5827dd50cefc60472ebb9a300064c7b0746b458a
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-24 14:22:59 +0300

    updated get heroes by id

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,28 +1,17 @@
-class HerosController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+
+class HerosController < ApplicationController
+
+    # GET /heroes
     def index 
-        heros = Hero.all 
-        render json: heros, status: :created
+        heros = Hero.all
+        render json: heros,status: :ok
     end
+
+    # GET /heroes/:id
     def show
         hero = Hero.find(params[:id])
-        render json: hero, serializer: :HeroPowerSerializer  , : status: :created
-    end
-    def destroy 
-        hero = find_hero
-        hero.destroy
-        head :no_content
+            render json: hero,status: :ok, serializer: HeroPowerSerializer
     end
 
-    private
-
-    def find_hero
-        Hero.find(params[:id])
-    end
-
-
-    def render_not_found_response
-        render json: { error: "Hero not found" }, status: :not_found
-    end
 end
