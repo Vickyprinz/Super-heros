0144341258aaa67e2d346fe95e6f5fcd03fd358c
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-24 14:21:00 +0300

    Update hero_powers_controller.rb

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,10 +1,12 @@
 class HeroPowersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     def index 
         hero_powers = HeroPower.all 
         render json: hero_powers
     end
+
     def create
         hero_power = HeroPower.create!(hero_power_params)
         power = Power.find(hero_power.power_id)
@@ -16,7 +18,7 @@ class HeroPowersController < ApplicationController
     def hero_power_params
         params.permit(:strength, :hero_id, :power_id)
     end
-
+    
     def render_not_found_response
         render json: { error: "HeroPowers not found" }, status: :not_found
     end
@@ -24,4 +26,5 @@ class HeroPowersController < ApplicationController
     def render_unprocessable_entity_response(invalid)
         render json: { errors: invalid.record.errors }, status: :unprocessable_entity
     end
+     
 end
